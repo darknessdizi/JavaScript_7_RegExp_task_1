65857128b70060f15bb2b4b4fe8e5fe7d0a4a094
Remezov Dmitriy <darknessdizi@gmail.com>
2023-09-27 00:43:25 +0300

    Initial project

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import Validator from './validator.js';
+
+const obj = new Validator('   vasy-A2_465f   ');
+console.log(obj);
---
+++ src/js/validator.js
@@ -0,0 +1,25 @@
+export default class Validator {
+    constructor(text) {
+        this.name = this.validateUsername(text);
+    }
+
+    validateUsername(text) {
+        const str = text.trim();
+        if (/^[a-zA-Z\-_0-9]+$/.test(str)) {
+            const regexp = /^(\d*\-*_*)|([0-9]{4})|(\d{0,1}|\-{0,1}|_{0,1})$/g;
+            const result = str.match(regexp);
+            if (result[0]) {
+                throw Error('Имя не должно начинаться цифрами, символами подчёркивания или тире');
+            }
+            if (result[1].length > 3) {
+                throw Error('Имя не должно содержать подряд более трёх цифр');
+            }
+            if (result[1]) {
+                throw Error('Имя не должно заканчиваться цифрами, символами подчёркивания или тире');
+            }
+            return str;
+        } else {
+            throw Error('Некорректное имя. Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
+        }
+    }
+}
